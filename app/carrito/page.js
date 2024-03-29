5f68ae98914fd98ae01933bba9f132383562de51
Salocin0 <nicolasarieldurelli@gmail.com>
2024-03-29 13:54:35 -0300

    color btn carrito

--- app/carrito/page.js
+++ app/carrito/page.js
@@ -7,7 +7,7 @@ const CarritoPage = () => {
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">No hay productos en el carrito</h1>
         <Link href="/">
-          <p className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <p className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
             Ir a inicio
           </p>
         </Link>
